3f822649d97cd3cfd5599c93daa28e9bace9223e
paradoxisme <you@example.com>
2026-07-08 14:55:20 +0200

    Use 6 direction

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,18 @@
 #include <string>
 #include <cctype>
 
-int checkW = KEY_W, checkA = KEY_A, checkS = KEY_S, checkD = KEY_D;
-std::string keyW = "W", keyA = "A", keyS = "S", keyD = "D";
+int checkQ = KEY_Q, checkW = KEY_W, checkE = KEY_E, checkA = KEY_A, checkS = KEY_S, checkD = KEY_D;
+std::string keyQ ="Q", keyW = "W", keyE = "E", keyA = "A", keyS = "S", keyD = "D";
 std::string message = "Current Layout (not detected):";
 
 extern "C" {
     EMSCRIPTEN_KEEPALIVE
-    void UpdateKeys(const char* w, const char* a, const char* s, const char* d) {
+    void UpdateKeys(const char* q, const char* w, const char* e, const char* a, const char* s, const char* d) {
         message = "Current Layout (detected):";
-        keyW = w; keyA = a; keyS = s; keyD = d;
+        keyQ = q; keyW = w; keyE = e; keyA = a; keyS = s; keyD = d;
+        checkQ = std::toupper(static_cast<unsigned char>(q[0]));
         checkW = std::toupper(static_cast<unsigned char>(w[0]));
+        checkE = std::toupper(static_cast<unsigned char>(e[0]));
         checkA = std::toupper(static_cast<unsigned char>(a[0]));
         checkS = std::toupper(static_cast<unsigned char>(s[0]));
         checkD = std::toupper(static_cast<unsigned char>(d[0]));
@@ -32,7 +34,9 @@ EM_JS(void, FetchKeyboardLayout_JS, (), {
         const get = (code) => layoutMap.get(code) || code[3];
         
         _UpdateKeys(
+            stringToNewUTF8(get("KeyQ")),
             stringToNewUTF8(get("KeyW")),
+            stringToNewUTF8(get("KeyE")),
             stringToNewUTF8(get("KeyA")),
             stringToNewUTF8(get("KeyS")),
             stringToNewUTF8(get("KeyD"))
@@ -51,9 +55,13 @@ void UpdateDrawFrame() {
     Color colorA = IsKeyDown(checkA) ? GREEN : MAROON;
     Color colorS = IsKeyDown(checkS) ? GREEN : MAROON;
     Color colorD = IsKeyDown(checkD) ? GREEN : MAROON;
+    Color colorQ = IsKeyDown(checkQ) ? GREEN : MAROON;
+    Color colorE = IsKeyDown(checkE) ? GREEN : MAROON;
     
     DrawText(message.c_str(), 100, 100, 20, DARKGRAY);
+    DrawText(keyQ.c_str(), 150, 150, 40, colorQ);
     DrawText(keyW.c_str(), 200, 150, 40, colorW);
+    DrawText(keyE.c_str(), 250, 150, 40, colorE);
     DrawText(keyA.c_str(), 150, 200, 40, colorA);
     DrawText(keyS.c_str(), 200, 200, 40, colorS);
     DrawText(keyD.c_str(), 250, 200, 40, colorD);
